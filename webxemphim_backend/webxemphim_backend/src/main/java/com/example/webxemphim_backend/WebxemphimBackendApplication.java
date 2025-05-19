5fb824b562afd2e2f9adc777b87373b09eb39ab7
DESKTOP-JC8STTK\admin <toanhd290803@gmail.com>
2025-05-19 11:24:27 +0700

    upload

---
+++ webxemphim_backend/webxemphim_backend/src/main/java/com/example/webxemphim_backend/WebxemphimBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.webxemphim_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebxemphimBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebxemphimBackendApplication.class, args);
+	}
+
+}
---
+++ webxemphim_backend/webxemphim_backend/src/test/java/com/example/webxemphim_backend/WebxemphimBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webxemphim_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebxemphimBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
